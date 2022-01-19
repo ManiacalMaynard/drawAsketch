7544ec49b13e4624d889921858990f8f6a52a2f2
ManiacalMaynard <maynard@maniacalindustries.com>
2022-01-18 17:37:25 -0800

    updated with variable sizing, improved reset, and some text

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // Ignore this console.log because, um, nothing to see here
 console.log("ignore the man behind the curtain");
 
-// Need to set a variable start point for the grid size slider deal
-let gridSize = 100;
-
+// Sets the value of the etch box element
 const container = document.getElementById('etchBox');
 
+// Sets the grid size according to the value of the range slider
+let gridSize = document.getElementById('ranger').value;
+
+// Sets the baseline color for drawing
+let drawColor = "hsl(200, 80%, 50%";
+
 function changeColor(event) {
-    event.target.style.backgroundColor = "#000";
+    event.target.style.backgroundColor = drawColor;
 };
 
+// Okay, let's create the grid, add mouseover and classname
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -18,14 +23,20 @@ function makeGrid(rows, cols) {
         box.addEventListener("mouseover", changeColor);
         container.appendChild(box).className = "gridbox";
     };
+    document.getElementById('rangerValue').innerText = gridSize;
+
 };
 
+// Evoke the grid maker!
 makeGrid(gridSize, gridSize);
 
+// Reset function to clear the current div background color and redraw the grid to the new ranger selected size
 let boxes = document.querySelectorAll('.gridbox');
 function resetGrid() {
-    console.log("reset");
     boxes.forEach(element => {
         element.style.backgroundColor = "#CCC";
     });
+    gridSize = document.getElementById('ranger').value;
+    makeGrid(gridSize, gridSize);
 };
+
